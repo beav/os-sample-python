221dc47c694836f849ac036f43060789e8ba2846
Chris Duryee <cduryee@redhat.com>
2021-08-04 18:55:04 -0400

    log test

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, request, current_app
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    return "XHello World!X"
+
+@application.route("/endpoint")
+def endpoint():
+    current_app.logger.warn("foo") 
     return "XHello World!X"
 
 if __name__ == "__main__":
